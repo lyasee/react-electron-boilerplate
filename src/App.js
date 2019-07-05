e4773c6283adeec2348f95163103cb7ed78f7fe5
smlee <lyasee@naver.com>
2019-07-05 14:46:11 +0900

    add sample dialog

--- src/App.js
+++ src/App.js
@@ -2,25 +2,45 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React1
-        </a>
-      </header>
-    </div>
-  );
-}
+const electronRemote = require("electron").remote;
+const { dialog } = electronRemote;
 
-export default App;
+const electron = require("electron");
+
+export default class App extends React.Component {
+  handleDialogClick = () => {
+    const properties = ["openFile", "openDirectory", "multiSelections"];
+    dialog.showOpenDialog({ properties });
+  };
+
+  handleMessageBoxShow = () => {
+    const options = {
+      type: "info",
+      buttons: ["Yes", "No", "Cancel"],
+      message: "Do you really want to quit?"
+    };
+
+    dialog.showMessageBox(options, res => {
+      console.log("res: ", res);
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a className="App-link" href="#" onClick={this.handleDialogClick}>
+            Dialog
+          </a>
+          <a className="App-link" href="#" onClick={this.handleMessageBoxShow}>
+            MessageBox
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
